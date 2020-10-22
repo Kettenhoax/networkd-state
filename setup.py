c62a012bf5394b7e7c4a1204873bcd92fa8c1e7c
Marcel Zeilinger <marcel.zeilinger@ait.ac.at>
2020-10-22 15:47:43 +0200

    Move [options.data_files] to setup.py
    options.data_files fails with latest setuptools
    Thus, move data_files options to setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 from setuptools import setup
-setup()
+setup(
+    data_files=[
+        ('lib/networkd_state', ['bin/networkd_state']),
+        ('/lib/systemd/system', ['networkd-state@.target']),
+    ],
+)
